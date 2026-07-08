d2a75fb2f78002852a08671623332e6490c370d0
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-07-08 14:03:39 +0200

    feat: i18n FR/EN translation + bump to 1.1.0

--- _meta.lua
+++ _meta.lua
@@ -3,5 +3,5 @@ return {
     name        = "hypersudoku",
     fullname    = _("Hyper Sudoku"),
     description = _("Sudoku with 4 extra inner regions"),
-    version     = "1.0.1",
+    version     = "1.1.0",
 }
